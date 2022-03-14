2de70a4687e469c07d3fbe9aec3ba7c653ee7331
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-14 12:09:18 -0400

    commit tree 49db0d9175b04e1e396a19b057a78a3e4bbdf57d

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'J Dog'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+let shortGreeting = `Welcome, ${currentUser[0]}!`
